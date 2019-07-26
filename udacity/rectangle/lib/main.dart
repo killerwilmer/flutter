4d2f15f26ab4e452ae120f9babb368d02dae0d81
Wilmer Arteaga <wilmer.arteaga@masivian.com>
2019-07-26 15:34:35 -0500

    Init project -> rectangle udacity

---
+++ udacity/rectangle/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HelloRectangle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: Colors.greenAccent,
+        height: 400.0,
+        width: 300,
+        child: Center(
+          child: Text(
+            'Hello!',
+            style: TextStyle(fontSize: 40.0, color: Colors.brown),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello Rectangle'),
+        ),
+        body: HelloRectangle(),
+      ),
+    ),
+  );
+}
