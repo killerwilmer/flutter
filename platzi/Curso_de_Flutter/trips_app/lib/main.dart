fa09a5d2cef9c196a8807bd4c63e90f3c7efd408
Wilmer Arteaga <killerwilmer@gmail.com>
2019-10-06 23:03:18 -0500

    Create description place. Title, stars and description.

--- platzi/Curso_de_Flutter/trips_app/lib/main.dart
+++ platzi/Curso_de_Flutter/trips_app/lib/main.dart
@@ -1,87 +1,31 @@
 import 'package:flutter/material.dart';
+import 'description_place.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  String descriptionDummy = "Lorem Ipsum is simply dummy text of the printing and typesetting "
+      "industry. Lorem Ipsum has been the industry's standard "
+      "dummy text ever since the 1500s, when an unknown printer "
+      "took a galley of type and scrambled it to make a type specimen book." + "\n\n"
+      "industry. Lorem Ipsum has been the industry's standard "
+      "dummy text ever since the 1500s, when an unknown printer "
+      "took a galley of type and scrambled it to make a type specimen book.";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack( // <-- STACK AS THE SCAFFOLD PARENT
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/images/spacex.png"), // <-- BACKGROUND IMAGE
-              fit: BoxFit.cover,
-            ),
-          ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("App title"),
         ),
-        Scaffold(
-          backgroundColor: Colors.transparent, // <-- SCAFFOLD WITH TRANSPARENT BG
-          body: Center(
-            child: Container(
-              width: double.infinity,
-              color: Colors.white.withOpacity(0.5),
-              child: Text("Wilmer",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.white, fontSize: 80.0, fontWeight: FontWeight.bold)),
-            )
-          ),
-        ),
-      ],
+        body: new DescriptionPlace("Bahamas", 4, descriptionDummy),
+      )
     );
   }
 }
