0990d15ae0c21d1017fdb099a62a9475c8a3cf51
Wilmer Arteaga <killerwilmer@gmail.com>
2019-10-20 22:32:40 -0500

    add review and image

---
+++ platzi/Curso_de_Flutter/trips_app/lib/review.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class Review extends StatelessWidget {
+
+  String pathImage = "assets/images/people.jpg";
+
+  @override
+  Widget build(BuildContext context) {
+    final photo = Container(
+      margin: EdgeInsets.only(
+        top: 20.0,
+        left: 20.0
+      ),
+
+      width: 80.0,
+      height: 80.0,
+
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage(pathImage)
+        )
+      ),
+    );
+
+    return Row(
+      children: <Widget>[
+
+      ],
+    );
+  }
+
+}
