ee3d761f281666040e45a86a96029ce810f1c106
Wilmer Arteaga <killerwilmer@gmail.com>
2019-10-22 23:12:24 -0500

    Added title to gradient

--- platzi/Curso_de_Flutter/trips_app/lib/main.dart
+++ platzi/Curso_de_Flutter/trips_app/lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
                 ReviewList()
               ],
             ),
-            GradientBack()
+            GradientBack("Popular")
           ],
         ),
       )
