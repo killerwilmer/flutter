7aa94c810f287b36af34964d05ce523b35667525
Wilmer Arteaga <killerwilmer@gmail.com>
2019-11-10 17:50:41 -0500

    integrate BLoC Singleton and providers
    install generic_bloc_provider

--- platzi/Curso-de-Flutter-Avanzado/places/lib/main.dart
+++ platzi/Curso-de-Flutter-Avanzado/places/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:places/User/model/bloc_user.dart';
 import 'package:places/trips_cupertino.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,23 +10,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-
-      ),
-      //home: Trips()
-      home: TripsCupertino(),
-    );
+    return BlocProvider(
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          home: TripsCupertino(),
+        ),
+        bloc: UserBLoc());
   }
 }
 
